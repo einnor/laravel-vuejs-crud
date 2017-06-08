1d4dbaf85d378831cd20673af191a0d9a9fd5818
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-06-08 17:24:38 +0300

    Added crud functionality on the back-end and added routes

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+
+    public function vueCrud()
+    {
+        return view('/vue-js-crud/index');
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $blogs = Blog::latest()->paginate(5);
+
+        $response = [
+            'pagination' => [
+                'total' => $blogs->total(),
+                'per_page' => $blogs->perPage(),
+                'current_page' => $blogs->currentPage(),
+                'latest_page' => $blogs->latestPage(),
+                'from' => $blogs->firstItem(),
+                'to' => $blogs->lastItem(),
+            ],
+            'data' => $blogs
+        ];
+
+        return response()->json($response);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+        $blog = Blog::create($request->all());
+
+        return response()->json($blog);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+        $blog = Blog::find($id)->update($request->all());
+
+        return response()->json($blog);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Blog::find($id)->delete();
+
+        return response()->json(['done']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => ['web']], function() {
+    Route::get('/vue-js-crud', 'BlogController@vueCrud');
+    Route::resource('/blogs', 'BlogController');
+});
